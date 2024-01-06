246c75ff5de195b1fb238acc3dc592bdd5ec56d2
DevLaukey <63729244+DevLaukey@users.noreply.github.com>
2024-01-06 09:42:03 +0300

    Landing page complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,35 @@
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
+import { Poppins } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { LoginButton } from "@/components/auth/login-button";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
 
 export default function Home() {
   return (
-    <Button variant={'secondary'}>
-      Clck me
-   </Button>
-  )
+    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+      <div className="space-y-6 text-center">
+        <h1
+          className={cn(
+            "text-6xl text-white font-semibold drop-shadow-md",
+            font.className
+          )}
+        >
+          Auth
+        </h1>
+        <p className="text-white">Auth Service</p>
+        <div className="">
+          <LoginButton>
+            <Button variant={"secondary"} size={"lg"}>
+              Sign In
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
+  );
 }
